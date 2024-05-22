6d7c5b2566109c163d3c5c5abbdea6a7a08dd898
yncute <dionedajalen@gmail.com>
2024-05-22 13:02:36 -0700

    Progress Bar visual changes, added isClicked useState to track

--- portfolio-website/src/components/ProgressBar.js
+++ portfolio-website/src/components/ProgressBar.js
@@ -2,9 +2,11 @@ import React, { useState, useRef } from "react";
 
 const ProgressBar = () => {
   const [progress, setProgress] = useState(0); //0-100
+  const [isClicked, setIsClicked] = useState(false);
   const progressBarRef = useRef(null);
 
   const handleMouseDown = (e) => {
+    setIsClicked(true);
     const updateProgress = (e) => {
       const rect = progressBarRef.current.getBoundingClientRect();
       const newProgress = Math.min(
@@ -21,6 +23,7 @@ const ProgressBar = () => {
     };
 
     const handleMouseUp = () => {
+      setIsClicked(false);
       window.removeEventListener("mousemove", handleMouseMove);
       window.removeEventListener("mouseup", handleMouseUp);
     };
@@ -36,27 +39,18 @@ const ProgressBar = () => {
       onMouseDown={handleMouseDown}
     >
       <div
-        className={`bg-white h-full rounded-full w-6/12 group-hover:bg-green-500 relative`}
+        className={`${
+          isClicked ? "bg-green-500" : "bg-white"
+        } h-full rounded-full group-hover:bg-green-500 relative`}
         style={{ width: `${progress}%` }}
       >
-        <div className="absolute right-0 top-1/2 transform translate-x-1/2 -translate-y-1/2 w-3 h-3 bg-white rounded-full opacity-0 group-hover:opacity-100 transition-opacity duration-0"></div>
+        <div
+          className={`absolute right-0 top-1/2 transform translate-x-1/2 -translate-y-1/2 w-3 h-3 bg-white rounded-full ${
+            isClicked ? "opacity-100" : "opacity-0"
+          } group-hover:opacity-100 transition-opacity duration-0`}
+        ></div>
       </div>
     </div>
-    // <div
-    //   ref={progressBarRef}
-    //   className="relative w-full h-4 bg-gray-300 rounded-lg cursor-pointer"
-    //   onMouseDown={handleMouseDown}
-    // >
-    //   <div
-    //     className="absolute h-full bg-blue-500 rounded-lg"
-    //     style={{ width: `${progress}%` }}
-    //   />
-    //   <div
-    //     className="absolute top-0 h-full w-4 bg-blue-700 rounded-full cursor-pointer"
-    //     style={{ left: `calc(${progress}% - 8px)` }}
-    //     onMouseDown={handleMouseDown}
-    //   />
-    // </div>
   );
 };
 
